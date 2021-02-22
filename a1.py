1cc79604febf6b2ca87e2fc1c73dd210bf62c339
cs18b047 <cs18b047@smail.iitm.ac.in>
2021-02-22 20:01:00 +0530

    Created .py file

---
+++ a1.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.10.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+print("hello world")
+
+
