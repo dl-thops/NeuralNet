7460fade8bb5fabeafb78b5c83ea255faf02f90e
cs18b047 <cs18b047@smail.iitm.ac.in>
2021-03-03 23:53:28 +0530

    added momentum and nesterov optimisers,xavier init

--- a1.py
+++ a1.py
@@ -19,10 +19,11 @@ from keras.datasets import fashion_mnist
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-#%%
+# %%
 import wandb
 wandb.login()
 wandb.init(project="fdl-a1")
+
 
 # %%
 class NeuralNet:
@@ -60,7 +61,7 @@ class NeuralNet:
     def addlayer( self, layer_size):
         self.structure = self.structure[:-1] + [ layer_size, self.structure[-1]]
         
-    def train( self,X,Y,numepochs = 100,learning_rate = 0.1,initialization_type = "random", activation = "sigmoid"):
+    def train( self,X,Y,numepochs = 100,learning_rate = 0.1,initialization_type = "random", activation = "sigmoid",optimiser = "vanilla",gamma=0.1):
         self.init_type = initialization_type
         self.activation = activation
         if self.init_type == "random":
@@ -68,17 +69,28 @@ class NeuralNet:
                 self.params["w"+str(i)]= np.random.rand( self.structure[i], self.structure[i-1])-0.5
                 self.params["b"+str(i)]= np.random.rand( self.structure[i], 1)-0.5
         elif self.init_type == "xavier":
-            #TODO implement xavier initialization
-            pass
+            for i in range(1,len(self.structure)):
+                self.params["w"+str(i)]= np.normal(0,1/sqrt(self.structure[i-1]+structure[i]),(self.structure[i],self.structure[i-1]))
+                self.params["b"+str(i)]= np.normal(0,1/sqrt(self.structure[i-1]+structure[i]),self.structure[i])
         else:
             print(self.init_type + ": unidentified initialization type")
         layers = len(self.structure)-1
         nsamples = X.shape[1]
         self.accuracies = []
+        if optimiser == "momentum" or optimiser == "nesterov":
+            update = {}
+            for i in range(1,layers+1):
+                update["w"+str(i)]=np.zeros(self.params["w"+str(i)].shape)
+                update["b"+str(i)]=np.zeros(self.params["b"+str(i)].shape)
         for epoch in tqdm(range(numepochs)):
             grads = {}
+            acc = self.prefict(X,returnclass=1)
+            if optimser == "nesterov":
+                for ii in range(1,layers+1):
+                    self.params["w"+str(ii)] -= gamma * update["w"+str(ii)]
+                    self.params["b"+str(ii)] -= gamma * update["b"+str(ii)]
             values = self.predict(X,returndict=1)
-            self.accuracies.append(np.mean(np.argmax(values["h"+str(layers)],axis=0) == Y))
+            self.accuracies.append(np.mean(acc == Y))
             wandb.log({"train_accuracy":self.accuracies[-1]})
             grads["a"+str(layers)] = -(np.eye(self.structure[-1])[Y]).T + values["h"+str(layers)]
             for ii in np.arange(layers-1,0,-1):
@@ -87,12 +99,22 @@ class NeuralNet:
             for ii in np.arange(layers,0,-1):
                 grads["w"+str(ii)] = np.matmul((grads["a"+str(ii)].T).reshape(nsamples,-1,1),(values["h"+str(ii-1)].T).reshape(nsamples,1,-1))
                 grads["b"+str(ii)] = grads["a"+str(ii)]
-            for ii in np.arange(1,layers+1):
-                self.params["w"+str(ii)] -= learning_rate * np.mean(grads["w"+str(ii)],axis=0)
-                self.params["b"+str(ii)] -= learning_rate * np.mean(grads["b"+str(ii)],axis=1).reshape(-1,1)
-    
+                
+            if optimiser == "vanilla":
+                for ii in np.arange(1,layers+1):
+                    self.params["w"+str(ii)] -= learning_rate * np.mean(grads["w"+str(ii)],axis=0)
+                    self.params["b"+str(ii)] -= learning_rate * np.mean(grads["b"+str(ii)],axis=1).reshape(-1,1)
+            elif optimiser == "momentum" or optimiser == "nesterov":
+                for ii in np.arange(1,layers+1):
+                    update["w"+str(ii)]=gamma * update["w"+str(ii)] + learning_rate * np.mean(grads["w"+str(ii)],axis=0)
+                    update["b"+str(ii)]=gamma * update["b"+str(ii)] + learning_rate * np.mean(grads["b"+str(ii)],axis=1).reshape(-1,1)
+                    self.params["w"+str(ii)] -= update["w"+str(ii)]
+                    self.params["b"+str(ii)] -= update["b"+str(ii)]
+        
+            
     
     def predict(self,X,returndict = 0,returnclass = 0):
+        #TODO : dont calculate dict unless returndict =1
         predictions = X
         values = {}
         values["h0"] = X
@@ -114,22 +136,22 @@ class NeuralNet:
         else:
             return values
 
-#%%
+# %%
 (X_train,Y_train),(X_test,Y_test) = fashion_mnist.load_data()
 
-#%%
+# %%
 X_train = X_train.reshape(X_train.shape[0],-1).T/256
 X_test = X_test.reshape(X_test.shape[0],-1).T/256
 
-#%%
+# %%
 wandb.config = {"dataset":"fashion_mnist","input_size":784,"output_size":10,"hidden_layers":[20,20,10],"epochs":60,\
-                     "learning_rate":2.5,"batch_size":None,"initialization_type":"random","activation":"sigmoid"}
+                     "learning_rate":2.5,"batch_size":None,"initialization_type":"random","activation":"sigmoid","optimiser":"nesterov","gamma":0.1}
 
-#%%
+# %%
 nn = NeuralNet(wandb.config["input_size"],wandb.config["output_size"])
 for hidden_layer_size in wandb.config["hidden_layers"]:
     nn.addlayer(hidden_layer_size)
 nn.train(X_train,Y_train,wandb.config["epochs"],wandb.config["learning_rate"],\
-         initialization_type=wandb.config["initialization_type"],activation=wandb.config["activation"])
+         initialization_type=wandb.config["initialization_type"],activation=wandb.config["activation"],optimiser=wandb.config["optimiser"],gamma=wandb.config["gamma"])
 
-#%%
+# %%
